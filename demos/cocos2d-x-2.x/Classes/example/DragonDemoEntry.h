cf47ba4c55dba7f3097a24fb1fc4f5f899132cd4
zrong <zrongzrong@gmail.com>
2014-04-22 12:00:47 +0800

    let DragonDemoEntry can move/jump/stop.

--- demos/cocos2d-x-2.x/Classes/example/DragonDemoEntry.h
+++ demos/cocos2d-x-2.x/Classes/example/DragonDemoEntry.h
@@ -8,8 +8,30 @@ class DragonDemoEntry : public DragonBonesDemo
 public:
 	DragonDemoEntry();
 	virtual std::string title();
+	virtual void onEnter();
+	virtual void update(float dt);
 
 	void animListener(CCNode*node, void*e);
+	void menuCallback(CCObject* pSender);
+	void moveLeftCallback(CCObject* pSender);
+	void moveRightCallback(CCObject* pSender);
+	void stopCallback(CCObject* pSender);
+	void jumpCallback(CCObject* pSender);
+	void changeClothesCallback(CCObject* pSender);
+
+private:
+	std::vector<std::string> _textures;
+	size_t _textureIndex;
+	int _moveDirection;
+	float _speedX;
+	float _speedY;
+	bool _isJumping;
+	float _footY;
+	CCSize _winSize;
+	CCDragonBones* _db;
+
+	void updateBehavior();
+	
 };
 
 #endif //__DRAGON_DEMO_ENTRY__
--- renderer/cocos2d-x-2.x/CCDragonBones.cpp
+++ renderer/cocos2d-x-2.x/CCDragonBones.cpp
@@ -11,141 +11,141 @@
 #include "DragonBonesHeaders.h"
 using namespace cocos2d;
 namespace dragonBones {
-  
-	 CCDragonBones*	CCDragonBones::create(const char* skeletonXMLFile,
-													  const char* dragonBonesName,
-                                                      const char* armatureName)
+
+	CCDragonBones*	CCDragonBones::create(const char* skeletonXMLFile,
+		const char* dragonBonesName,
+		const char* armatureName)
 	{
-		return CCDragonBones::create( skeletonXMLFile,
-                                           "texture.xml",
-                                            dragonBonesName,
-                                            armatureName,
-                                            "");
+		return CCDragonBones::create(skeletonXMLFile,
+			"texture.xml",
+			dragonBonesName,
+			armatureName,
+			"");
 	}
- 
-															 
-   CCDragonBones*	CCDragonBones::create(const char* skeletonXMLFile,
-                                                             const char* textureXMLFile,
-                                                             const char* dragonBonesName,
-                                                             const char* armatureName,
-                                                             const char* animationName)
+
+
+	CCDragonBones*	CCDragonBones::create(const char* skeletonXMLFile,
+		const char* textureXMLFile,
+		const char* dragonBonesName,
+		const char* armatureName,
+		const char* animationName)
 	{
 		CCDragonBones* pNew = new CCDragonBones();
-        if(pNew && pNew->init())
-        {
-			Armature*arm = CCDragonBones::buildArmature( skeletonXMLFile,
-                                           textureXMLFile,
-                                            dragonBonesName,
-                                            armatureName,
-                                            animationName);
-            pNew->initWithArmature(arm);
-            pNew->autorelease();
-            return pNew;
-        }
-        CC_SAFE_DELETE(pNew);
-        return NULL;
+		if (pNew && pNew->init())
+		{
+			Armature*arm = CCDragonBones::buildArmature(skeletonXMLFile,
+				textureXMLFile,
+				dragonBonesName,
+				armatureName,
+				animationName);
+			pNew->initWithArmature(arm);
+			pNew->autorelease();
+			return pNew;
+		}
+		CC_SAFE_DELETE(pNew);
+		return NULL;
 	}
-    
-    CCDragonBones* CCDragonBones::create(Armature*arm)
-    {
-        CCDragonBones* pNew = new CCDragonBones();
-        if(pNew && pNew->init())
-        {
-            pNew->initWithArmature(arm);
-            pNew->autorelease();
-            return pNew;
-        }
-        CC_SAFE_DELETE(pNew);
-        return NULL;
-    }
-    void CCDragonBones::update(float dt)
-    {
-        if(m_Armature)
-            m_Armature->advanceTime(dt);
-    }
-    
-    CCNode*  CCDragonBones::getDisplayNode(){
-        return static_cast<CocosNode*>(m_Armature->getDisplay())->node;
-    }
-    
-    Armature*  CCDragonBones::getArmature()
-    {
-        return m_Armature;
-    }
-	
-    void  CCDragonBones::addEventListener(const String &type,const String &key, cocos2d::CCObject*pObj,SEL_CallFuncND callback)
-    {  
+
+	CCDragonBones* CCDragonBones::create(Armature*arm)
+	{
+		CCDragonBones* pNew = new CCDragonBones();
+		if (pNew && pNew->init())
+		{
+			pNew->initWithArmature(arm);
+			pNew->autorelease();
+			return pNew;
+		}
+		CC_SAFE_DELETE(pNew);
+		return NULL;
+	}
+	void CCDragonBones::update(float dt)
+	{
+		if (m_Armature)
+			m_Armature->advanceTime(dt);
+	}
+
+	CCNode*  CCDragonBones::getDisplayNode(){
+		return static_cast<CocosNode*>(m_Armature->getDisplay())->node;
+	}
+
+	Armature*  CCDragonBones::getArmature()
+	{
+		return m_Armature;
+	}
+
+	void  CCDragonBones::addEventListener(const String &type, const String &key, cocos2d::CCObject*pObj, SEL_CallFuncND callback)
+	{
 		m_Caller = pObj;
 		m_Callback = callback;
-		std::function<void(Event*)> f =  std::bind(&CCDragonBones::eventBridge, this,std::placeholders::_1); 
+		std::function<void(Event*)> f = std::bind(&CCDragonBones::eventBridge, this, std::placeholders::_1);
 		m_Armature->addEventListener(type, f, key);
-    }
+	}
 
-	
-		bool  CCDragonBones:: hasEventListener(const String &type)
-		{
-			return m_Armature->hasEventListener(type);
-		}
-		void CCDragonBones::removeEventListener(const String &type, const std::string &key)
-		{
-			m_Armature->removeEventListener(type,key);
-		}
-		void CCDragonBones::dispatchEvent(Event *event)
-		{
-			m_Armature->dispatchEvent(event);
-		}
-  void CCDragonBones::eventBridge(Event*e){ 
-	  (m_Caller->*m_Callback)(this,e);   
-  }
-    void CCDragonBones::gotoAndPlay(
-                const String &animationName,
-                Number fadeInTime ,
-                Number duration ,
-                Number loop ,
-                uint layer ,
-                const String &group ,
-                const String &fadeOutMode ,
-                bool displayControl ,
-                bool pauseFadeOut ,
-                bool pauseFadeIn
-                )
-    {
-        m_Armature->getAnimation()->gotoAndPlay(
-                                                animationName,
-                                                fadeInTime ,
-                                                duration ,
-                                                loop ,
-                                                layer ,
-                                                group ,
-                                                fadeOutMode ,
-                                                displayControl ,
-                                                pauseFadeOut ,
-                                                pauseFadeIn
-                                                );
-    }
-    
-    
-    void CCDragonBones::onExit()
-    {
-        this->unscheduleAllSelectors();
-    }
-    
-    Armature*   CCDragonBones::buildArmature(const char* skeletonXMLFile,
-                                           const char* textureXMLFile,
-                                           const char* dragonBonesName,
-                                           const char* armatureName,
-                                           const char* animationName)
-    {
-        Cocos2dxFactory fac;
+
+	bool  CCDragonBones::hasEventListener(const String &type)
+	{
+		return m_Armature->hasEventListener(type);
+	}
+	void CCDragonBones::removeEventListener(const String &type, const std::string &key)
+	{
+		m_Armature->removeEventListener(type, key);
+	}
+	void CCDragonBones::dispatchEvent(Event *event)
+	{
+		m_Armature->dispatchEvent(event);
+	}
+	void CCDragonBones::eventBridge(Event*e){
+		(m_Caller->*m_Callback)(this, e);
+	}
+	void CCDragonBones::gotoAndPlay(
+		const String &animationName,
+		Number fadeInTime,
+		Number duration,
+		Number loop,
+		uint layer,
+		const String &group,
+		const String &fadeOutMode,
+		bool displayControl,
+		bool pauseFadeOut,
+		bool pauseFadeIn
+		)
+	{
+		m_Armature->getAnimation()->gotoAndPlay(
+			animationName,
+			fadeInTime,
+			duration,
+			loop,
+			layer,
+			group,
+			fadeOutMode,
+			displayControl,
+			pauseFadeOut,
+			pauseFadeIn
+			);
+	}
+
+
+	void CCDragonBones::onExit()
+	{
+		this->unscheduleAllSelectors();
+	}
+
+	Armature*   CCDragonBones::buildArmature(const char* skeletonXMLFile,
+		const char* textureXMLFile,
+		const char* dragonBonesName,
+		const char* armatureName,
+		const char* animationName)
+	{
+		Cocos2dxFactory fac;
 		fac.loadSkeletonFile(skeletonXMLFile);
 		fac.loadTextureAtlasFile(textureXMLFile);
-        return fac.buildArmature(armatureName, animationName ,dragonBonesName);
-    }
-    
-    void CCDragonBones::initWithArmature(Armature*arm)
-    {
-        this->m_Armature = arm;
-        this->schedule(schedule_selector(CCDragonBones::update), 0);
+		return fac.buildArmature(armatureName, animationName, dragonBonesName);
+	}
+
+	void CCDragonBones::initWithArmature(Armature*arm)
+	{
+		this->m_Armature = arm;
+		this->schedule(schedule_selector(CCDragonBones::update), 0);
 		this->addChild(static_cast<CocosNode*>(m_Armature->getDisplay())->node);
-    }
+	}
 }
