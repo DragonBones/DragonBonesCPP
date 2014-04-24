f02f9c8677491b6fb266df395213069548334b3e
Jiangzhou He <hejiangzhou@gmail.com>
2014-04-23 21:37:03 -0700

    Refactor Makefile for Android.

--- renderer/cocos2d-x-2.x/Android.mk
+++ renderer/cocos2d-x-2.x/Android.mk
@@ -4,12 +4,7 @@ LOCAL_MODULE := dragonbones_cocos2dx2_static
 
 LOCAL_MODULE_FILENAME := libdragonbones-cocos2dx2
 
-LOCAL_SRC_FILES := \
-	CCDragonBones.cpp \
-	Cocos2dxAtlasNode.cpp \
-	Cocos2dxDisplayBridge.cpp \
-	Cocos2dxFactory.cpp \
-	Cocos2dxTextureAtlas.cpp \
+LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/*.cpp)
 
 # TODO(hejiangzhou): Shall we disable exception?
 LOCAL_CPPFLAGS := -fexceptions -std=c++11
@@ -30,8 +25,3 @@ include $(BUILD_STATIC_LIBRARY)
 
 $(call import-module,cocos2dx)
 $(call import-module,dragonbones)
-$(call import-module,CocosDenshion/android)
-$(call import-module,extensions)
-$(call import-module,external/Box2D)
-$(call import-module,external/chipmunk)
-
