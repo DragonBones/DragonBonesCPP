a4fa61c18e765ae00fe9055dc41b48e107ea96a1
lite3 <lite3@qq.com>
2014-08-07 19:35:04 +0800

    add DBCCArmatureNode to DBCCRenderHeaders

--- demos/cocos2d-x-3.x/Classes/AppDelegate.cpp
+++ demos/cocos2d-x-3.x/Classes/AppDelegate.cpp
@@ -1,6 +1,7 @@
 #include "AppDelegate.h"
 #include "DemoDragonBoy.h"
 #include "DemoKnight.h"
+#include "HelloWorldScene.h"
 
 USING_NS_CC;
 
@@ -28,7 +29,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     director->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
-    auto scene = DemoKnight::createScene();
+    auto scene = HelloWorld::createScene();
 
     // run
     director->runWithScene(scene);
