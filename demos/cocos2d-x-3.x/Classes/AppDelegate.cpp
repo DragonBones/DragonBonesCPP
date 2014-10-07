1e181020b1f85aa24c3a344b3f2e7ff884bdcc38
zrong <zrongzrong@gmail.com>
2014-10-07 09:56:11 +0800

    update ios_mac project

--- demos/cocos2d-x-3.x/Classes/AppDelegate.cpp
+++ demos/cocos2d-x-3.x/Classes/AppDelegate.cpp
@@ -26,7 +26,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0 / 60);
 
-    auto layer = DemoBase::create(1);
+    auto layer = DemoBase::create(0);
     // create a scene. it's an autorelease object
     Scene* scene = Scene::create();
     scene->addChild(layer);
