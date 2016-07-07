a222d0cbcd795fb259bac694fe30d0d6a3cf7851
Nguyen Dac Nam <nam.kazt.91@gmail.com>
2016-07-07 14:12:29 +0700

    update android.mk to fit with new folder struct

--- Cocos2DX_3.x/src/dragonBones/cocos2dx/proj.android/Android.mk
+++ Cocos2DX_3.x/src/dragonBones/cocos2dx/proj.android/Android.mk
@@ -7,53 +7,53 @@ LOCAL_MODULE := dragonbones_static
 LOCAL_MODULE_FILENAME := libdragonbones
 
 
-LOCAL_SRC_FILES := ../../animation/Animation.cpp \
-                   ../../animation/AnimationState.cpp \
-                   ../../animation/TimelineState.cpp \
-                   ../../animation/WorldClock.cpp \
-                   ../../armature/Armature.cpp \
-                   ../../armature/Bone.cpp \
-                   ../../armature/Slot.cpp \
-                   ../../core/BaseObject.cpp \
-                   ../../events/EventObject.cpp \
-                   ../../factories/BaseFactory.cpp \
-                   ../../model/AnimationData.cpp \
-                   ../../model/ArmatureData.cpp \
-                   ../../model/DragonBonesData.cpp \
-				   ../../model/FrameData.cpp \
-				   ../../model/TimelineData.cpp \
-                   ../../parsers/DataParser.cpp \
-                   ../../parsers/JSONDataParser.cpp \
-                   ../../textures/TextureData.cpp \
-                   ../../project/CCArmatureDisplayContainer.cpp \
-                   ../../project/CCFactory.cpp \
-                   ../../project/CCSlot.cpp \
-                   ../../project/CCTextureData.cpp
+LOCAL_SRC_FILES := ../animation/Animation.cpp \
+                   ../animation/AnimationState.cpp \
+                   ../animation/TimelineState.cpp \
+                   ../animation/WorldClock.cpp \
+                   ../armature/Armature.cpp \
+                   ../armature/Bone.cpp \
+                   ../armature/Slot.cpp \
+                   ../core/BaseObject.cpp \
+                   ../events/EventObject.cpp \
+                   ../factories/BaseFactory.cpp \
+                   ../model/AnimationData.cpp \
+                   ../model/ArmatureData.cpp \
+                   ../model/DragonBonesData.cpp \
+		   ../model/FrameData.cpp \
+		   ../model/TimelineData.cpp \
+                   ../parsers/DataParser.cpp \
+                   ../parsers/JSONDataParser.cpp \
+                   ../textures/TextureData.cpp \
+                   ../cocos2dx/CCArmatureDisplayContainer.cpp \
+                   ../cocos2dx/CCFactory.cpp \
+                   ../cocos2dx/CCSlot.cpp \
+                   ../cocos2dx/CCTextureData.cpp
 
 
-LOCAL_C_INCLUDES :=     $(LOCAL_PATH)/../../ \
-						$(LOCAL_PATH)/../../animation \
-                        $(LOCAL_PATH)/../../armature \
-                        $(LOCAL_PATH)/../../core \
-                        $(LOCAL_PATH)/../../events \
-                        $(LOCAL_PATH)/../../factories \
-                        $(LOCAL_PATH)/../../geom \
-                        $(LOCAL_PATH)/../../model \
-                        $(LOCAL_PATH)/../../parsers \
-                        $(LOCAL_PATH)/../../project \
-                        $(LOCAL_PATH)/../../textures \
+LOCAL_C_INCLUDES :=     $(LOCAL_PATH)/../ \
+			$(LOCAL_PATH)/../animation \
+                        $(LOCAL_PATH)/../armature \
+                        $(LOCAL_PATH)/../core \
+                        $(LOCAL_PATH)/../events \
+                        $(LOCAL_PATH)/../factories \
+                        $(LOCAL_PATH)/../geom \
+                        $(LOCAL_PATH)/../model \
+                        $(LOCAL_PATH)/../parsers \
+                        $(LOCAL_PATH)/../cocos2dx \
+                        $(LOCAL_PATH)/../textures \
 
-LOCAL_EXPORT_C_INCLUDES :=     $(LOCAL_PATH)/../../ \
-						$(LOCAL_PATH)/../../animation \
-                        $(LOCAL_PATH)/../../armature \
-                        $(LOCAL_PATH)/../../core \
-                        $(LOCAL_PATH)/../../events \
-                        $(LOCAL_PATH)/../../factories \
-                        $(LOCAL_PATH)/../../geom \
-                        $(LOCAL_PATH)/../../model \
-                        $(LOCAL_PATH)/../../parsers \
-                        $(LOCAL_PATH)/../../project \
-                        $(LOCAL_PATH)/../../textures \
+LOCAL_EXPORT_C_INCLUDES :=     $(LOCAL_PATH)/../ \
+			$(LOCAL_PATH)/../animation \
+                        $(LOCAL_PATH)/../armature \
+                        $(LOCAL_PATH)/../core \
+                        $(LOCAL_PATH)/../events \
+                        $(LOCAL_PATH)/../factories \
+                        $(LOCAL_PATH)/../geom \
+                        $(LOCAL_PATH)/../model \
+                        $(LOCAL_PATH)/../parsers \
+                        $(LOCAL_PATH)/../cocos2dx \
+                        $(LOCAL_PATH)/../textures \
 
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_static
 
