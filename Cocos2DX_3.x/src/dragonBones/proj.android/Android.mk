44362a1ef743b8d16016d4f6c7fc33898c9e9225
akdcl <akdcl@sina.com>
2016-07-07 19:19:49 +0800

    modify ArmatureDisplayContainer name

--- Cocos2DX_3.x/src/dragonBones/proj.android/Android.mk
+++ Cocos2DX_3.x/src/dragonBones/proj.android/Android.mk
@@ -7,32 +7,32 @@ LOCAL_MODULE := dragonbones_static
 LOCAL_MODULE_FILENAME := libdragonbones
 
 
-LOCAL_SRC_FILES := ../animation/Animation.cpp \
-                   ../animation/AnimationState.cpp \
-                   ../animation/TimelineState.cpp \
-                   ../animation/WorldClock.cpp \
-                   ../armature/Armature.cpp \
-                   ../armature/Bone.cpp \
-                   ../armature/Slot.cpp \
-                   ../core/BaseObject.cpp \
-                   ../events/EventObject.cpp \
-                   ../factories/BaseFactory.cpp \
-                   ../model/AnimationData.cpp \
-                   ../model/ArmatureData.cpp \
-                   ../model/DragonBonesData.cpp \
-		   ../model/FrameData.cpp \
-		   ../model/TimelineData.cpp \
-                   ../parsers/DataParser.cpp \
-                   ../parsers/JSONDataParser.cpp \
-                   ../textures/TextureData.cpp \
-                   ../cocos2dx/CCArmatureDisplayContainer.cpp \
-                   ../cocos2dx/CCFactory.cpp \
-                   ../cocos2dx/CCSlot.cpp \
-                   ../cocos2dx/CCTextureData.cpp
+LOCAL_SRC_FILES :=      ../animation/Animation.cpp \
+                        ../animation/AnimationState.cpp \
+                        ../animation/TimelineState.cpp \
+                        ../animation/WorldClock.cpp \
+                        ../armature/Armature.cpp \
+                        ../armature/Bone.cpp \
+                        ../armature/Slot.cpp \
+                        ../core/BaseObject.cpp \
+                        ../events/EventObject.cpp \
+                        ../factories/BaseFactory.cpp \
+                        ../model/AnimationData.cpp \
+                        ../model/ArmatureData.cpp \
+                        ../model/DragonBonesData.cpp \
+                        ../model/FrameData.cpp \
+                        ../model/TimelineData.cpp \
+                        ../parsers/DataParser.cpp \
+                        ../parsers/JSONDataParser.cpp \
+                        ../textures/TextureData.cpp \
+                        ../cocos2dx/CCArmatureDisplay.cpp \
+                        ../cocos2dx/CCFactory.cpp \
+                        ../cocos2dx/CCSlot.cpp \
+                        ../cocos2dx/CCTextureData.cpp
 
 
 LOCAL_C_INCLUDES :=     $(LOCAL_PATH)/../ \
-			$(LOCAL_PATH)/../animation \
+                        $(LOCAL_PATH)/../animation \
                         $(LOCAL_PATH)/../armature \
                         $(LOCAL_PATH)/../core \
                         $(LOCAL_PATH)/../events \
@@ -44,7 +44,7 @@ LOCAL_C_INCLUDES :=     $(LOCAL_PATH)/../ \
                         $(LOCAL_PATH)/../textures \
 
 LOCAL_EXPORT_C_INCLUDES :=     $(LOCAL_PATH)/../ \
-			$(LOCAL_PATH)/../animation \
+                        $(LOCAL_PATH)/../animation \
                         $(LOCAL_PATH)/../armature \
                         $(LOCAL_PATH)/../core \
                         $(LOCAL_PATH)/../events \
